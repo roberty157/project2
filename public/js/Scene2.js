4edff9b3ba52d69eca4f86b1c9d0b2333e41cbe4
Robert <robertyeam@gmail.com>
2021-06-26 12:28:08 -0700

    added sprite

--- public/js/Scene2.js
+++ public/js/Scene2.js
@@ -42,18 +42,24 @@ class Scene2 extends Phaser.Scene{
         //console.log(platform1.x = 700);
         //console.log(Object.entries(platforms));
         // The player and its settings
-        player = this.physics.add.sprite(100, 450, 'dude');
 
+        player = this.physics.add.sprite(100, 450, 'OurSprite');
+        player.setScale(.4);
         player.setGravityY(250);
+
+        player.body.useDamping=true;
+        player.setDrag(0.95,0);
 
         //  Player physics properties. Give the little guy a slight bounce.
         player.setBounce(0.2);
         player.setCollideWorldBounds(true);
 
         //  Our player animations, turning, walking left and walking right.
+        //0,2
+        //3,5
         this.anims.create({
             key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 2 }),
+            frames: this.anims.generateFrameNumbers('OurSprite', { start: 3, end: 5 }),
             frameRate: 10,
             repeat: -1
         });
@@ -66,7 +72,7 @@ class Scene2 extends Phaser.Scene{
 */
         this.anims.create({
             key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', { start: 3, end: 5 }),
+            frames: this.anims.generateFrameNumbers('OurSprite', { start: 0, end: 2 }),
             frameRate: 10,
             repeat: -1
         });
@@ -129,13 +135,14 @@ class Scene2 extends Phaser.Scene{
 
             player.anims.play('right', true);
         }
+        /*
         else
         {
             player.setVelocityX(0);
 
             player.anims.play('turn');
         }
-
+        */
         if (cursors.up.isDown && player.body.touching.down)
         {
             player.setVelocityY(-330);
